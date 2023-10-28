99b8806ed28de77d099af417a333973da3eb1f55
curegit <37978051+curegit@users.noreply.github.com>
2023-10-28 22:03:04 +0900

    Prohibit from instantiating directly

--- reification/core.py
+++ reification/core.py
@@ -7,8 +7,18 @@ class Reified:
 
     type_args: tuple[type | Any, ...] = (Any,)
 
+    def __new__(cls, *args, **kwargs):
+        # Prohibit from instantiating directly
+        if cls is Reified:
+            raise RuntimeError("Cannot instantiate 'Reified' class directly.")
+        return super().__new__(cls, *args, **kwargs)
+
     # Return type should be inferred
     def __class_getitem__(cls, params: Any):
+        # Prohibit from instantiating directly
+        if cls is Reified:
+            raise RuntimeError("Cannot instantiate 'Reified' class directly.")
+        # Returns a separated reified type
         param_tuple = tuplize_class_getitem_params(params)
         rt = get_reified_type(cls, param_tuple)
         rt.targ = params
