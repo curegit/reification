f09d6a9af87116864d48a2cebbf71261dd800a3d
curegit <37978051+curegit@users.noreply.github.com>
2023-11-06 12:57:23 +0900

    Add docstring

--- reification/core.py
+++ reification/core.py
@@ -3,9 +3,29 @@ from .utils import get_reified_type, tuplize_class_getitem_params
 
 
 class Reified:
-    targ: Any = type | Any
+    """Mixin class designed to facilitate the creation of new types based on reified type parameters"""
+
+    targ: type | tuple[type | Any, ...] | Any = Any
+    """
+    This class property represents the type argument(s) that were specified when the reified generic class was instantiated.
+    If there is more than one type argument, `targ` will be a tuple containing each given type.
+    If no type argument is specified, `Any` will be returned.
+
+    Returns:
+        type | tuple[type | Any, ...] | Any: The type argument(s) given when the class was instantiated.
+        If no type argument was given, `Any` will be returned.
+    """
 
     type_args: tuple[type | Any, ...] = (Any,)
+    """
+    This class property holds the type argument(s) provided for the reified generic class.
+    Unlike `targ`, `type_args` always returns a tuple of the specified type arguments, even when there's only one type argument.
+    If no type arguments are given, it contains `Any`.
+
+    Returns:
+        tuple[type | Any, ...]: A tuple containing the type argument(s) given when the class was instantiated.
+        If no type argument was given, the returned tuple contains `Any`.
+    """
 
     def __new__(cls, *args, **kwargs):
         # Prohibit from instantiating directly
